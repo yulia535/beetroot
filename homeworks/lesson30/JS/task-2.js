fe138f3573b67ec4071a78895166527d6537c31a
Yulia Sukhar <yulia-s@i.ua>
2023-11-29 13:57:44 +0200

    types

---
+++ homeworks/lesson30/JS/task-2.js
@@ -0,0 +1,6 @@
+// Виконай додавання рядка "1" і цифри 2 (обидві операнди повинні бути в змінних), добийся математично правильної відповіді.
+const stringType = '1';
+const numberType = 2;
+const result = numberType + Number.parseInt(stringType);
+
+export {result};
